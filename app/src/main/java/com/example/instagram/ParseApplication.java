a888680397d148580dca1d3105abb776fdeb78d7
flaurencyac <flaurencya@fb.com>
2021-07-05 22:35:38 -0400

    initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.instagram;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Register your parse models
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("OMzOIzdYwwdPkIkjDBzgSG5c54GhZ4uCzGukAQNK")
+                .clientKey("EZQNxeFHw5OQx6y8JoNGhY1J8ru8NJu1XFRnKxZl")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
